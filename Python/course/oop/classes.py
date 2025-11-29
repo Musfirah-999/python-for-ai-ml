af45ddd3a77c43aff70de1deb6c79e4fc2bab383
MusfirahZainab <zainabmusfirah.00@gmail.com>
2025-11-29 23:09:34 +0500

    oop_start

---
+++ Python/course/oop/classes.py
@@ -0,0 +1,11 @@
+class Student:
+    name = "John Doe"
+    age = 20
+    
+student1 = Student()
+print("Student Name:", student1.name)
+student2 = Student()
+student2.name = "Jane Smith"
+
+print("Student Name:", student2.name)
+

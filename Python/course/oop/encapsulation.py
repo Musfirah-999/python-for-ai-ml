a2914529617db44cac8e3e275f067fdaac6be63c
MusfirahZainab <zainabmusfirah.00@gmail.com>
2025-12-02 17:05:37 +0500

    Encapsulation

--- Python/course/oop/encapsulation.py
+++ Python/course/oop/encapsulation.py
@@ -1,35 +1,35 @@
 class Account:
     def __init__(self,account_holder, balance,):
         self.account_holder = account_holder
-        self.balance = balance
+        self.__balance = balance
         self.transactions = []
     
     
     def deposit(self,amount):
         if amount <= 0:
             print("Deposit amount must be positive.")
-            return self.balance
-        self.balance += amount
+            return self.__balance
+        self.__balance = self.__balance + amount
         self.transactions.append(f"Deposited: ${amount}")
         print(f"Deposited: ${amount}")
-        return self.balance
+        return self.__balance
     
     
     def withdraw(self, amount):
        
        if amount <= 0:
             print("Withdrawal amount must be positive.")
-            return self.balance
-       elif amount > self.balance:
+            return self.__balance
+       elif amount > self.__balance:
             print("Insufficient balance.")
-            return self.balance
-       self.balance -= amount
+            return self.__balance
+       self.__balance -= amount
        self.transactions.append(f"Withdrawn: ${amount}")
        print(f"Withdrawn: ${amount}")
-       return self.balance
+       return self.__balance
    
     def check_balance(self):
-        return f"Account Number: {self.account_holder}, Balance: {self.balance}"
+        print( f"Account Number: {self.account_holder}, Balance: ${self.__balance}")
     def transection_history(self):
         if self.transactions == []:
             print("No transactions yet.")
@@ -37,6 +37,8 @@ class Account:
         print(f"--Transaction History for {self.account_holder}'s Account---")
         for transaction in self.transactions:
             print(f"\t{transaction}")
+    def get_balance(self):
+        return self.__balance
             
 class BankSystem:
     def __init__(self):
@@ -46,12 +48,23 @@ class BankSystem:
         if name in self.accounts:
             print(f"Account {name} already exists")
         else:
-            self.accounts[name] = Account(initial_balance, name)
+            self.accounts[name] = Account(name, initial_balance)
         
     def  get_account(self, name) -> Account:
         return self.accounts.get(name, None)
 
 bank = BankSystem()
 bank.create_account("David", 25)
-               
+david_account = bank.get_account("David")
+if david_account:
+    david_account.deposit(25)
+    david_account.deposit(10)
+    david_account.withdraw(15)
+    david_account.check_balance()
+    david_account.transection_history()
+    # david_account.__balance = 100 
+    david_account.transection_history()
+    david_account.check_balance()
+    print(david_account.get_balance())
+    
     

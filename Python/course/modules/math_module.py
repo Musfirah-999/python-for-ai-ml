99b7ef23a18807dc6c8273f3629af932880523b4
MusfirahZainab <zainabmusfirah.00@gmail.com>
2025-12-07 15:14:28 +0500

    math module

---
+++ Python/course/modules/math_module.py
@@ -0,0 +1,28 @@
+import math
+
+
+#math constants
+# print(math.pi)
+# print(math.e)
+# print(math.tau)
+# print(math.nan)
+# print(math.inf)
+
+
+# print(math.fabs(-10))  #absolute value
+# print(math.pow(2,3))  
+# print(math.sqrt(25))  
+# print(math.factorial(6))  
+# print(math.gcd(18,20))  
+
+
+# print(math.floor(4.9))
+# print(math.ceil(4.1))
+# print(round(4.2))
+# print(round(4.7))
+
+
+def circle_area(r):
+    return math.pi *math.pow(r,2)
+
+print(circle_area(5))
